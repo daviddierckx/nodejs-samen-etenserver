490efbf9a84f64a7e97772787811c51f7f09d825
daviddierckx <david.dierckx@hotmail.com>
2021-04-23 12:15:40 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const http = require('http')
 
-const hostname = '127.0.0.1'
+//const hostname = '127.0.0.1'
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
+  console.log('request started')
     let result = {
-        'response': 'Hello World!',
-        'status': 'Alles OK!'
+        response: 'Hello World!',
+        status: 'Alles OK!'
     }
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify(result))
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
 })

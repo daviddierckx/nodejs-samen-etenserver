f0c8b8fa86c39e272b3388b1b86448add02af09e
daviddierckx <david.dierckx@hotmail.com>
2021-04-25 22:25:51 +0200

    Added server changes

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('api/info'));
-
 app.get('/api/info', (req, res) => {
   console.log("Get request op /api/info")
   

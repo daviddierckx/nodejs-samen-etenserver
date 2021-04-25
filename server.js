5018b9caf030b050a4038eb5ecc7d64eb35a335e
David Dierckx <david.dierckx@hotmail.com>
2021-04-25 21:59:43 +0200

    updated server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/api/info', (req, res) => {
   console.log("Get request op /api/info")
   
   const info = {

e4c4f96e90c0a95009888e3f5007650a204d0369
David Dierckx <david.dierckx@hotmail.com>
2021-04-26 14:23:21 +0200

    database added

---
+++ database.js
@@ -0,0 +1,5 @@
+let db = []
+
+module.exports = {
+    db
+}
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const database = require('./database')
+
+var logger = require('tracer').console()
+
+console.log(database.db)
+
 const port = process.env.PORT || 3000
 
 app.get('/api/info', (req, res) => {
-  console.log("Get request op /api/info")
+  logger.log("Get request op /api/info")
   
   const info = {
     Studentnaam: 'David Dierckx',
@@ -15,5 +21,5 @@ app.get('/api/info', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at :${port}`)
+  logger.log(`Example app listening at :${port}`)
 })

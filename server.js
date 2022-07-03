fc104aa6980349366a5c012c7f034da67a74f6f3
David Dierckx <david.dierckx@hotmail.com>
2022-07-03 23:57:42 +0200

    ss

--- server.js
+++ server.js
@@ -15,9 +15,33 @@ app.get('/', (req, res) => {
     res.send("Sharemeal API");
 });
 
+app.use(express.json())
 app.use('/api', routes);
 
 
+app.get('/api/info', (req, res) => {
+    logger.log("Get request op /api/info")
+
+    const info = {
+        Studentnaam: 'David Dierckx',
+        Studentnummer: '2179946',
+        Beschrijving: 'Avans hogeschool',
+        SonarqubeURL: 'https://sonarqube.avans-informatica-breda.nl/dashboard?id=nodejs-sameneten'
+    }
+    res.status(200).json(info)
+})
+// Catch-all route
+app.all("*", (req, res, next) => {
+    console.log("Catch-all endpoint aangeroepen");
+    next({ message: "Endpoint '" + req.url + "' does not exist", errCode: 404 });
+});
+app.use((error, req, res, next) => {
+    console.log("Errorhandler called! ", error);
+    res.status(error.errCode).json({
+        error: "Some error occurred",
+        message: error.message,
+    });
+});
 app.listen(port, () => {
     logger.log(`Avans app listening at http://localhost:${port}`);
 });

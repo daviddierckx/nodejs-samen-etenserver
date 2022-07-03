03d8ea56fa839300265714824cbed6aeba1762f0
David Dierckx <david.dierckx@hotmail.com>
2022-07-03 23:36:50 +0200

    push

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const app = express();
 const routes = require('./app/router/routes')
 const logger = require('tracer').console()
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+const dotenv = require('dotenv')
+
+app.get('/', (req, res) => {
+    res.send("Sharemeal API");
+});
 
 app.use('/api', routes);
+
+
 app.listen(port, () => {
     logger.log(`Avans app listening at http://localhost:${port}`);
 });
